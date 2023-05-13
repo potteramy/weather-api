188d3e49868298b3294a250cbb4a0455f5f870e2
Potter <potteramylynn@gmail.com>
2023-05-13 18:54:29 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Weather API
 ## About
+In this project I created a weather dashboard web app using the Open Weather API. Users can search by city to display the current weather and five day forecast. Local storage is used to store previous searches for easy trip planning. 
 ## Screenshot
+![Here is a screenshot of the result page.](./assets/images/_C__Users_potte_bootcamp_homework_weather-api_index.html_.png)
 ## Links
+Open [https://potteramy.github.io/weather-api/](https://potteramy.github.io/weather-api/) with your browser to see the result.
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,29 @@ var button = $("#submit-button");
 var cityName;
 var todayWeather = $(".todayWeather");
 var fiveDay = $(".fiveDay");
+var searches = $('.searches')
 var cityHistory;
 var apiKey = "70ddba253090d64cb533d825e655e603"
+
 function getHistory() {
   cityHistory = JSON.parse(localStorage.getItem("cities"));
-
   if (cityHistory === null) {
     cityHistory = [];
+  }
+  historyButtons()
+}
+
+function historyButtons() {
+  searches.html('')
+  for(var i = 0; i < cityHistory.length; i++) {
+    let name = cityHistory[i]
+    let button = $('<button class="btn btn-primary">')
+    button.text(name)
+    searches.append(button)
+    button.on('click', function(e) {
+      event.preventDefault();
+      getWeather(name);
+    })
   }
 }
 
@@ -21,30 +37,30 @@ function historySearch(event) {
 }
 
 function getWeather(cityName) {
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey)
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey+"&units=imperial")
     .then((response) => response.json())
     .then(function (data) {
       console.log(data);
       var fiveDayHtml = ''; 
       for(var i = 0; i < data.list.length; i+=8) {
        
-        fiveDayHtml += `<p>Temperature: ${data.list[i].main.temp} degrees K </p>
+        fiveDayHtml += `<div class='card'><img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png"><p>Temperature: ${data.list[i].main.temp} degrees F </p>
         <p>Humidity: ${data.list[i].main.humidity}%</p>
-        <p>Wind Speed:${data.list[i].wind.speed}</p>
+        <p>Wind Speed: ${data.list[i].wind.speed}mph</p></div>
         `;
       }
       fiveDay.html(`<h2>Five Day:</h2>${fiveDayHtml}`);
     });
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid="+ apiKey)
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid="+ apiKey + "&units=imperial")
     .then((response) => response.json())
     .then(function (data) {
       console.log(data);
       todayWeather.html(`<h2>${
         data.name
       } (${dayjs.unix(data.dt).format("MM/DD/YYYY")}) <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></h2>
-    <p>Temperature: ${data.main.temp} degrees K </p>
+    <p>Temperature: ${data.main.temp} degrees F </p>
     <p>Humidity: ${data.main.humidity}%</p>
-    <p>Wind Speed:${data.wind.speed}</p>`);
+    <p>Wind Speed: ${data.wind.speed}mph</p>`);
     })
     
 }
@@ -57,5 +73,11 @@ function getWeather(cityName) {
 button.on("click", function (event) {
   event.preventDefault();
   var cityName = $('#search-bar').val()
+  if (!cityHistory.includes(cityName)) {
+    cityHistory.push(cityName)
+    localStorage.setItem('cities', JSON.stringify(cityHistory))
+    historyButtons()
+  }
   getWeather(cityName);
 });
+getHistory();
